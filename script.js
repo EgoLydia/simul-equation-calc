cc2902b3c5f3ad5198bbbaebc8221c2e9b1a5a37
Ego Lydia <94656569+EgoLydia@users.noreply.github.com>
2022-05-14 15:53:43 +0100

    Merge branch 'main' of https://github.com/EgoLydia/simul-equation-calc.

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ let x2Input = document.getElementById("x2");
 let y2Input = document.getElementById("y2");
 let c2Input = document.getElementById("c2");
 let solve = document.getElementById("solve");
+let dot = document.getElementById("dot");
 let decimalAdded = false;
 let activeInput = undefined;
 let sign = document.getElementById("sign");
@@ -21,20 +22,29 @@ c2Input.addEventListener("focus", onFocus);
 sign.addEventListener("click", switchSign);
 solve.addEventListener("click", solveEquation);
 clearItem.addEventListener("click", clear);
-
+dot.addEventListener("click", dotNum);
 function solveEquation() {
-  let x1 = x1Input.value;
-  let y1 = y1Input.value;
-  let c1 = c1Input.value;
-  let x2 = x2Input.value;
-  let y2 = y2Input.value;
-  let c2 = c2Input.value;
-
+  let x1 = parseFloat(x1Input.value);
+  let y1 = parseFloat(y1Input.value);
+  let c1 = parseFloat(c1Input.value);
+  let x2 = parseFloat(x2Input.value);
+  let y2 = parseFloat(y2Input.value);
+  let c2 = parseFloat(c2Input.value);
+  if (
+    isNaN(x1) ||
+    isNaN(y1) ||
+    isNaN(c1) ||
+    isNaN(x2) ||
+    isNaN(y2) ||
+    isNaN(c2)
+  ) {
+    finalResult.innerHTML = "some invalid inputs";
+    return;
+  }
   let x = (c2 * y1 - c1 * y2) / (y1 * x2 - x1 * y2);
   let y = (c1 * x2 - c2 * x1) / (y1 * x2 - x1 * y2);
   finalResult.innerHTML = "x =" + x + " y=" + y;
 }
-
 function onFocus(e) {
   activeInput = e.target;
 }
@@ -57,11 +67,19 @@ function switchSign() {
 }
 
 function clear() {
-  x1Input.value ="";
-  y1Input.value="";
-  c1Input.value ="";
-  x2Input.value ="";
-  y2Input.value ="";
-  c2Input.value ="";
-  finalResult.innerHTML =""
+  x1Input.value = "";
+  y1Input.value = "";
+  c1Input.value = "";
+  x2Input.value = "";
+  y2Input.value = "";
+  c2Input.value = "";
+  finalResult.innerHTML = "";
+}
+
+function dotNum(e) {
+  if (!activeInput) return;
+
+  if (activeInput.value.indexOf(".") === -1) {
+    activeInput.value += e.target.innerHTML;
+  }
 }
